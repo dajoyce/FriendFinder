aede323f186c98ae4ca7f37495e5d846bfeaec19
Dee Jay <42320330+dajoyce@users.noreply.github.com>
2019-02-15 21:03:40 -0500

    A little closer to done

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,4 +6,39 @@ module.exports = function(app) {
   app.get("/api/friends", function(req, res) {
     res.json(friends);
   });
+  var bestMatch = {
+    name: "",
+    photo: "",
+    friendDifference: 1000
+  };
+  console.log(req.body);
+
+  var userData = req.body;
+  var userScores = userData.scores;
+
+  console.log(userScores);
+
+  var totalDifference = 0;
+
+  //Use a nested for loop to go through friend possibilities and see who is the closest match
+  for (var i = 0; 1 < friends.length; i++) {
+    console.log(friends[i]);
+    totalDifference = 0;
+
+    for (var j = 0; j < friends[i].scores[j]; j++) {
+      totalDiffence += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
+
+      if (totalDifference <= bestMatch.friendDifference) {
+        bestMatch.name = friends[i].name;
+        bestMatch.photo = friends[i].photos;
+        bestMatch.friendDifference = totalDifference;
+      }
+    }
+  }
+
+  //Saves user data into the database (Done after the check so the user doesn't match with themselves)
+  friends.push(userData);
+
+  //return the best match for the user in JSON format
+  res.json(bestMatch);
 };
